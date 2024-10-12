fdea50f815a70fa0535eab0b05201e91ae21aa2e
Ajay19-amp <132136871+Ajay19-amp@users.noreply.github.com>
2024-10-12 15:24:11 +0000

    this is a vairables of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountEmail= "ajaykumar19899@gmail.com"
+var accountPassword ="ajay1230"
+let accountCity= "kashipur"
+accountName ="karan"
+// account2
+
+
+accountCity="rampur"
+accountPassword="akaka"
+accountName="lallan"
+console.table([accountCity,accountEmail,accountPassword,accountName])
